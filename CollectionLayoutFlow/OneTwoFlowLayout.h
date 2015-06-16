d650425e046b3efa8c3305b61ba04b6043341e20
Zian Chen <ansonchen1986@macs-iMac-4.home>
2015-06-16 07:19:26 -0400

    initialzation

---
+++ CollectionLayoutFlow/OneTwoFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  OneTwoFlowLayout.h
+//  CollectionLayoutFlow
+//
+//  Created by Zian Chen on 6/15/15.
+//  Copyright (c) 2015 Zian Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OneTwoFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ CollectionLayoutFlow/OneTwoFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  OneTwoFlowLayout.m
+//  CollectionLayoutFlow
+//
+//  Created by Zian Chen on 6/15/15.
+//  Copyright (c) 2015 Zian Chen. All rights reserved.
+//
+
+#import "OneTwoFlowLayout.h"
+
+@implementation OneTwoFlowLayout
+
+@end
